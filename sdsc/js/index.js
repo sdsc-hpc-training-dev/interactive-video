3dd30dc0db1a3d15d52631652668d0524ab85d96
Yuu6883 <okumura6883@gmail.com>
2021-06-19 19:58:46 -0700

    Image path fix

--- sdsc/js/index.js
+++ sdsc/js/index.js
@@ -1,10 +1,10 @@
-const loadScript = async (id, src) => {
+const loadScript = async (id, src, root) => {
     const res = await fetch(src);
-    document.getElementById(id).innerHTML = await res.text();
+    document.getElementById(id).innerHTML = (await res.text()).replace('{ROOT}', root);
 };
 
 window.onload = () => {
     const root = document.getElementById('web-root').textContent;
-    loadScript('header', `${root}/sdsc/header.html`);
-    loadScript('footer', `${root}/sdsc/footer.html`);
+    loadScript('header', `${root}/sdsc/header.html`, root);
+    loadScript('footer', `${root}/sdsc/footer.html`, root);
 };
