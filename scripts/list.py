187b25004a7899fde21a4d12132a5fa929db44e0
Yuu6883 <okumura6883@gmail.com>
2021-08-23 15:39:28 -0700

    Token test

--- scripts/list.py
+++ scripts/list.py
@@ -9,7 +9,7 @@ from googleapiclient.http import MediaIoBaseDownload
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-# from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.readonly']
@@ -34,10 +34,11 @@ def main():
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
-        # else:
-        #     flow = InstalledAppFlow.from_client_secrets_file(
-        #         'credentials.json', SCOPES)
-        #     creds = flow.run_local_server(port=0)
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+            
         # # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
