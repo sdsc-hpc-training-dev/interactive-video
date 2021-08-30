4a5185d69402558cafcc8daed5c7fbc962579213
Yuu6883 <okumura6883@gmail.com>
2021-08-30 14:05:31 -0700

    Only import app flow when needed

--- scripts/list.py
+++ scripts/list.py
@@ -11,13 +11,11 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.readonly']
 
 def main():
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--verbose", help="Increase output verbosity", action="store_true")
@@ -59,8 +57,10 @@ def main():
             creds.refresh(Request())
         else:
             if not os.path.exists('credentials.json'):
-                print('"credentials.json" missing')
+                print(f'{colors.FAIL}"credentials.json" missing{colors.ENDC}')
                 return
+            from google_auth_oauthlib.flow import InstalledAppFlow
+
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
