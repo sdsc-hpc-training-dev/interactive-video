3678d569dd0288c27cae8f7dfb85658fce023e51
Yuu6883 <okumura6883@gmail.com>
2021-09-16 17:30:46 -0700

    Use sdsc include header & footer

--- sdsc/js/index.js
+++ sdsc/js/index.js
@@ -1,10 +1,9 @@
-const loadScript = async (id, src, root) => {
+const loadScript = async (id, src) => {
     const res = await fetch(src);
-    document.getElementById(id).innerHTML = (await res.text()).replace(/\{ROOT\}/g, root);
+    document.getElementById(id).innerHTML = await res.text();
 };
 
 window.onload = () => {
-    const root = document.getElementById('web-root').textContent;
-    loadScript('header', `${root}/sdsc/header.html`, root);
-    loadScript('footer', `${root}/sdsc/footer.html`, root);
+    loadScript('header', 'https://www.sdsc.edu/_include/header.html');
+    loadScript('footer', 'https://www.sdsc.edu/_include/footer.html');
 };
