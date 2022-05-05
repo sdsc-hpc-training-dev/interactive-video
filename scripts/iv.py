98fe6e50561198d987eb9b6b1eb5332e30eb79e8
Yuu <38842891+Yuu6883@users.noreply.github.com>
2022-05-05 16:41:38 -0700

    Update iv.py
    Added arguments for init series & add video to series

--- scripts/iv.py
+++ scripts/iv.py
@@ -11,6 +11,8 @@ import requests
 import argparse
 import xml.etree.ElementTree as ET
 
+from datetime import datetime
+
 SAMPLE_CONFIG = {
     "title": "Sample Title",
     "subtitle": "Sample subtitle",
@@ -28,8 +30,34 @@ SAMPLE_CONFIG = {
     "date": "1/1/2022"
 }
 
+LIST_PHP = """
+<?php
+
+if (file_exists("./page.json")) {
+    $res = (array) json_decode(file_get_contents("./page.json"));
+} else $res = array();
+
+$res['list'] = array();
+foreach (glob("./*/config.json") as $filename) {
+    $config = file_get_contents($filename);
+    $json = json_decode($config, true);
+    $obj = array();
+    $obj['path'] = explode("/", $filename)[1];
+    $obj['title'] = $json['title'];
+    $obj['subtitle'] = $json['subtitle'];
+    $obj['description'] = $json['description'];
+    $obj['time'] = strtotime($json['date']);
+    array_push($res['list'], $obj);
+}
+
+echo json_encode($res);
+
+?>
+"""
+
+
 def sync_events():
-    res = requests.get("https://www.sdsc.edu/news_and_events/events.xml")
+    res = requests.get("https://sdsc.edu/education_and_training/training_hpc.xml")
     resText = res.text
     root = ET.fromstring(resText)
 
@@ -43,13 +71,14 @@ def sync_events():
         name = page_node.find("name").text
         title = page_node.find("display-name").text
 
-        start = page_node.find("start-date")
-        start_ts = int(start.text) if start is not None else None
-
-        end = page_node.find("end-date")
-        end_ts = int(end.text) if end is not None else None
-
         item_node = page_node.find("system-data-structure")
+        
+        start = item_node.find("start_date")
+        start_ts = int(start.text) // 1000 if start is not None else None
+
+        end = item_node.find("end_date")
+        end_ts = int(end.text) // 1000 if end is not None else None
+
         desc_long_node = item_node.find("description_long/p")
         long = ET.tostring(desc_long_node, encoding="unicode", method="html") if desc_long_node else None
         short = item_node.find("description_short").text
@@ -110,22 +139,52 @@ def build_list_html():
         item = events[key]
         link = item["vid_link"]
 
+        start = datetime.fromtimestamp(item["start"])
+
+        if start > datetime.now():
+            continue
+
+        end = datetime.fromtimestamp(item["end"])
+
+        month_str = start.strftime("%b") if start.month == end.month else start.strftime("%b") + "-" + end.strftime("%b")
+        day_str = start.day if start.day == end.day else str(start.day) + "-" + str(end.day)
+        year_str = start.year if start.year == end.year else str(start.year) + "-" + str(end.year)
+
+        content = ""
+
+        time_box = f"""
+            <div class="event-box">
+                <span class="event-month">{month_str}</span>
+                <span class="event-day">{day_str}</span>
+                <span class="event-year">{year_str}</span>
+            </div>
+        """
+
         if link is None:
-            sections.append(f"""
+            content = f"""
                 <h2>{item["title"]}</h2>
                 <p>
                     <em>{item["desc"]["short"] or item["desc"]["long"]}</em>
                 </p>
-                """)
+                """
         else:
-            sections.append(f"""
+            content = f"""
             <h2>
                 <a href="{link}">{item["title"]}</a>
             </h2>
             <p>
                 <em>{item["desc"]["short"] or item["desc"]["long"]}</em>
             </p>
-            """)
+            """
+
+        sections.append(f"""
+        <li class="event">
+            {time_box}
+            <div class="event-details">
+                {content}
+            </div>
+        </li>
+        """)
 
     with open("dev.html", "w", encoding="utf8") as out:
         out.write(html.replace("{PAGES}", "</hr>".join(sections)))
@@ -142,13 +201,55 @@ def init_webinar(folder_name):
         os.mkdir(folder_name)
     except:
         print(f"{colors.WARNING}Folder already exists{colors.ENDC}")
+        return
         
     shutil.copy2("webinar.html", os.path.join(folder_name, "index.html"))
 
     with open(os.path.join(folder_name, "config.json"), "w") as f:
-        json.dump(SAMPLE_CONFIG, f)
+        json.dump(SAMPLE_CONFIG, f, indent=4)
 
     print(f"{colors.OKGREEN}Webinar created: {colors.ENDC}{colors.UNDERLINE}https://education.sdsc.edu/training/interactive/{folder_name}{colors.ENDC}")
+
+def init_series(folder_name, title):
+    # enforce naming convention here?
+
+    try:
+        os.mkdir(folder_name)
+    except:
+        print(f"{colors.WARNING}Folder already exists{colors.ENDC}")
+        return
+        
+    shutil.copy2("series_list.html", os.path.join(folder_name, "index.html"))
+
+    with open(os.path.join(folder_name, "page.json"), "w") as f:
+        json.dump({ "title": title }, f, indent=4)
+
+    with open(os.path.join(folder_name, "list.php"), "w") as f:
+        f.write(LIST_PHP)
+
+    print(f"{colors.OKGREEN}Series created: {colors.ENDC}{colors.UNDERLINE}https://education.sdsc.edu/training/interactive/{folder_name}{colors.ENDC}")
+
+def add_series(series_name, folder_name):
+
+    if not os.path.exists(series_name):
+        print(f"{colors.FAIL}Series does not exists{colors.ENDC}")
+        return
+
+    vid_path = os.path.join(series_name, folder_name)
+    
+    try:
+        os.mkdir(vid_path)
+    except:
+        print(f"{colors.WARNING}Folder already exists{colors.ENDC}")
+        return
+
+    shutil.copy2("series.html", os.path.join(vid_path, "index.html"))
+    
+    with open(os.path.join(vid_path, "page.json"), "w") as f:
+        json.dump(SAMPLE_PAGE, f, indent=4)
+
+    print(f"{colors.OKGREEN}Added video to{colors.ENDC}{colors.OKCYAN}{series_name}: " +
+        f"{colors.ENDC}{colors.UNDERLINE}https://education.sdsc.edu/training/interactive/{series_name}/{folder_name}{colors.ENDC}")
 
 class colors:
     OKCYAN = '\033[96m'
@@ -175,6 +276,9 @@ def main():
     parser.add_argument("--sync", help="Sync events from SDSC main site", action="store_true")
     parser.add_argument("--build", help="Build list page from local event json databases", action="store_true")
     parser.add_argument("--webinar", help="Initialize a folder for a webinar", type=str)
+    parser.add_argument("--series", help="Initialize a folder for a series, --title or --add is required", type=str)
+    parser.add_argument("--init", help="Init a video series with a title (depends on --series)", type=str)
+    parser.add_argument("--add", help="Add a video to the series (depends on --series)", type=str)
 
     args = parser.parse_args()
 
@@ -186,6 +290,17 @@ def main():
 
     if args.webinar:
         init_webinar(args.webinar)
+
+    if args.series:
+        if args.init:
+            init_series(args.series, args.init)
+        elif args.add:
+            add_series(args.series, args.add)
+        else:
+            print(f"{colors.WARNING}Either --add or --init is required with --series{colors.ENDC}")
+    else:
+        if args.init or args.add:
+            print("--series [series_name] is required")
     
 if __name__ == "__main__":
     
